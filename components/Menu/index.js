1a2fe3755b95b2516e0a7b5ae394d7fad368ede4
HARSH KUMAR <harshkumarsingh54321@gmail.com>
2024-06-14 14:16:50 +0530

    added communication between toolbox and menu

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -2,14 +2,24 @@ import { FaPencilAlt,FaUndo,FaRedo,FaEraser } from "react-icons/fa";
 import { FaFileArrowDown } from "react-icons/fa6";
 
 import styles from './index.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick,actionItemClick } from "@/slice/menuSlice";
+
 const Menu = () =>{
+    const dispatch = useDispatch();
+    const activeMenuItem = useSelector((state)=>state.menu.activeMenuItem)
+    const handleMenuClick = (itemName) =>{
+        dispatch(menuItemClick(itemName))
+    }
+
     return (
-        <div className={styles.menuContainer} >
-            <div className={styles.iconsWrapper}>
-                <FaPencilAlt className={styles.icon} />
+            <div className={styles.menuContainer} >
+            <div className={`${styles.iconsWrapper} ${activeMenuItem === MENU_ITEMS.PENCIL ? styles.active : ''}`} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+            <FaPencilAlt className={styles.icon} />
             </div>
-            <div className={styles.iconsWrapper}>
-                <FaEraser className={styles.icon} />
+            <div className={`${styles.iconsWrapper} ${activeMenuItem === MENU_ITEMS.ERASER ? styles.active : ''}`} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+            <FaEraser className={styles.icon} />
             </div>
             <div className={styles.iconsWrapper}>
                 <FaUndo className={styles.icon} />
