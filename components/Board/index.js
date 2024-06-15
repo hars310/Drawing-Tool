4d3f7d0c734026f949c1b6351cf2483faedd9043
HARSH KUMAR <harshkumarsingh54321@gmail.com>
2024-06-15 18:07:59 +0530

    undo and redo completed

--- components/Board/index.js
+++ components/Board/index.js
@@ -9,6 +9,8 @@ const Board = () =>{
     const canvasRef = useRef(null);
     const dispatch = useDispatch();  // for changing the actionmenuitem to null to use download button again and again
     const shouldDraw = useRef(false); 
+    const drawHistory = useRef([]);
+    const historyPointer = useRef(0);
     const {activeMenuItem,actionMenuItem} = useSelector((state)=>state.menu);
     const {color,size} = useSelector((state)=> state.toolbox[activeMenuItem])
 
@@ -23,6 +25,16 @@ const Board = () =>{
             anchor.href = URL
             anchor.download = 'sketch.jpg'
             anchor.click();
+        }else if(actionMenuItem===MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO){
+                if(historyPointer.current>0 && actionMenuItem===MENU_ITEMS.UNDO){
+                        historyPointer.current -= 1;
+                }
+                if( historyPointer.current<drawHistory.current.length-1 && actionMenuItem===MENU_ITEMS.REDO){
+                    historyPointer.current += 1;
+                }
+                const imageData = drawHistory.current[historyPointer.current]
+                context.putImageData(imageData,0,0)
+
         }
         dispatch(actionItemClick(null)) // changes the actionmenuItem from "DOWNLOAD" to NULL, so that useEffect can rerender the component
 
@@ -71,6 +83,9 @@ const Board = () =>{
         }
         const handleMouseUp = (e) =>{
             shouldDraw.current = false;
+            const imageData = context.getImageData(0,0,canvas.width,canvas.height) 
+            drawHistory.current.push(imageData)
+            historyPointer.current = drawHistory.current.length-1;
         }
 
         canvas.addEventListener('mousedown',handleMouseDown);
